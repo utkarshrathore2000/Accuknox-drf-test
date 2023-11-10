8dfb083c16b80ae8b5641bdd1b0b1ee4a639a58e
utkarsh <utkarshrathore6102@gmail.com>
2023-11-10 14:07:47 +0530

    Update postgres_database name variable

--- social_media/social_media/settings.py
+++ social_media/social_media/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = "social_media.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.environ.get("POSTGRES_NAME"),
+        "NAME": os.environ.get("POSTGRES_DB"),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
         "HOST": os.environ.get("POSTGRES_HOST"),
